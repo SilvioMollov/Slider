03124b238b929a04428f6f8092f95c0223f1a381
SilvioMollov <33893512+SilvioMollov@users.noreply.github.com>
2020-03-02 20:12:11 +0200

    working on autoplay

--- src/index.js
+++ src/index.js
@@ -1,19 +1,79 @@
 import "./style.css";
-import './../node_modules/@fortawesome/fontawesome-free/css/all.css'
+import "./../node_modules/@fortawesome/fontawesome-free/css/all.css";
 // import printMe from "./print.js";
-
+// let theFirstChild = parentElement.firstChild;
+let parentElement = document.querySelector(".carousel-container");
 const sliderContainer = document.querySelector(".carousel-slide");
 const sliderImages = document.querySelectorAll(".carousel-slide img");
 
-const prevButton = document.getElementById("arrow-left");
-const nextButton = document.getElementById("arrow-right");
+let sliderImagesArr = Array.from(sliderImages)
+console.log(sliderImagesArr)
 
 let numImg = sliderImages.length;
-console.log(numImg)
+console.log(numImg);
 
 let counter = 0;
-const imgSize = sliderImages[0].clientWidth; //size of the im
+const imgSize = sliderImages[0].clientWidth;
+
+function toggleArrows() {
+  if (parentElement.dataset.arrows === "true") {
+    function leftArrow() {
+      let leftArrow = document.createElement("i");
+      parentElement.appendChild(leftArrow);
+      console.log(leftArrow);
+      leftArrow.className = "fas fa-chevron-left";
+      leftArrow.id = "arrow-left";
+
+      const prevButton = document.getElementById("arrow-left");
+      prevButton.addEventListener("click", () => {
+        slideLeft();
+        infinityToLast();
+      });
+    }
+
+    leftArrow();
+
+    function rightArrow() {
+      console.log(parentElement);
+
+      let rightArrow = document.createElement("i");
+      parentElement.appendChild(rightArrow);
+      console.log(rightArrow);
+      rightArrow.className = "fas fa-chevron-right";
+      rightArrow.id = "arrow-right";
+
+      const nextButton = document.getElementById("arrow-right");
+      nextButton.addEventListener("click", () => {
+        console.log(" length " + sliderImages[counter]);
+
+        slideRight();
+        infinityToFirst();
+      });
+    }
+
+    rightArrow();
+  }
+}
+
+toggleArrows();
+
+//size of the im
 // sliderContainer.style.transform = 'translateX(' + (-imgSize) + 'px'
+
+function autoPlay() {
+    if (parentElement.dataset.autoplay === "true") {
+      for (let i = 0; i < numImg - 1; i++) {
+          setTimeout(() => {
+            counter++;
+            sliderContainer.style.transform = "translateX(" + -imgSize * counter + "px";
+          }, 5000)
+      }
+       
+    }
+  
+}
+
+autoPlay();
 
 function slideRight() {
   sliderContainer.style.transition = "transform 0.4s ease-in-out";
@@ -33,20 +93,6 @@ function slideLeft() {
   console.log(counter);
 }
 
-console.log(nextButton);
-
-nextButton.addEventListener("click", () => {
-  console.log(" length " + sliderImages[counter]);
-
-  slideRight();
-  infinityToFirst();
-});
-
-prevButton.addEventListener("click", () => {
-  slideLeft();
-  infinityToLast();
-});
-
 // function infinityToFirst() {
 //   if (typeof sliderImages[counter] === "undefined") {
 //     console.log(" length " + sliderImages.length);
@@ -57,8 +103,8 @@ prevButton.addEventListener("click", () => {
 // }
 
 function infinityToFirst() {
-  if ( counter === numImg) {
-    console.log('prev');
+  if (counter === numImg) {
+    console.log("prev");
 
     counter = sliderImages.length - numImg;
     sliderContainer.style.transform = "translateX(" + -imgSize * counter + "px";
@@ -66,10 +112,12 @@ function infinityToFirst() {
 }
 
 function infinityToLast() {
-  if ( counter === -1) {
-    console.log('prev');
+  if (counter === -1) {
+    console.log("prev");
 
     counter = sliderImages.length - 1;
     sliderContainer.style.transform = "translateX(" + -imgSize * counter + "px";
   }
 }
+
+// let theFirstChild = parentElement.firstChild;
