285acf9e6dff73f35b18161b7491318280aa7c3b
Silvio Mollov <sisomollov290@gmail.com>
2020-03-13 01:14:22 +0200

    one function

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
-import "./style.css";
-import "./../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import './style.css';
+import './../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
-let container;
+let container = document.querySelector('.s-slider'),
+  track = document.createElement('div');
+
 let slides;
 let slidesCount;
 let counter;
 let slideSize;
-let track;
 
-const stringToBoolean = dataValue => dataValue === "true";
+const stringToBoolean = dataValue => dataValue === 'true';
 
-function wrap() {
-  container = document.querySelector(".s-slider");
-  track = document.createElement("div");
+window.onload = MySlider(track, container);
+
+function MySlider(wrapper, container) {
+
 
   container.childNodes.forEach(slide => {
     if (slide.nodeType === 1) {
-      slide.classList.add("slide");
-      track.appendChild(slide);
+      slide.classList.add('slide');
+      wrapper.appendChild(slide);
     }
   });
 
-  track.classList = "carousel-track";
-  container.innerHTML = "";
-  container.appendChild(track);
-}
+  wrapper.classList = 'carousel-track';
+  container.innerHTML = '';
+  container.appendChild(wrapper);
 
-
-function MySlider() {
-  wrap();
-  slides = track.childNodes;
+  slides = wrapper.childNodes;
   slideSize = -slides[0].clientWidth;
   slidesCount = slides.length;
   counter = 0;
@@ -40,57 +38,54 @@ function MySlider() {
   if (container.dataset.autoplay) {
     autoPlay(container.dataset.autoplay);
   }
-}
 
-window.onload = MySlider();
+  function toggleArrows() {
+    let leftArrow = document.createElement('i');
+    container.appendChild(leftArrow);
+    leftArrow.className = 'fas fa-chevron-left';
+    leftArrow.id = 'arrow-left';
 
-// creating a function that creates the arrow accepting an argument, based on which arrow we are setting
-function toggleArrows() {
-  let leftArrow = document.createElement("i");
-  container.appendChild(leftArrow);
-  leftArrow.className = "fas fa-chevron-left";
-  leftArrow.id = "arrow-left";
+    const prevButton = document.getElementById('arrow-left');
+    prevButton.addEventListener('click', () => {
+      slidingOn('left');
+    });
 
-  const prevButton = document.getElementById("arrow-left");
-  prevButton.addEventListener("click", () => {
-    slidingOn("left");
-  });
+    let rightArrow = document.createElement('i');
+    container.appendChild(rightArrow);
+    rightArrow.className = 'fas fa-chevron-right';
+    rightArrow.id = 'arrow-right';
 
-  let rightArrow = document.createElement("i");
-  container.appendChild(rightArrow);
-  rightArrow.className = "fas fa-chevron-right";
-  rightArrow.id = "arrow-right";
+    const nextButton = document.getElementById('arrow-right');
+    nextButton.addEventListener('click', () => {
+      slidingOn('right');
+    });
+  }
+  function autoPlay(delay) {
+    setInterval(() => {
+      slidingOn('right');
+    }, delay);
+  }
 
-  const nextButton = document.getElementById("arrow-right");
-  nextButton.addEventListener("click", () => {
-    slidingOn("right");
-  });
-}
+  function slidingOn(direction) {
+    wrapper.style.transition = 'transform 0.4s ease-in-out';
+    if (direction == 'right') {
+      counter++;
+      wrapper.style.transform = 'translateX(' + slideSize * counter + 'px';
 
-function autoPlay(delay) {
-  setInterval(() => {
-    slidingOn("right");
-  }, delay);
-}
+      if (counter === slidesCount) {
+        counter = slides.length - slidesCount;
+        wrapper.style.transform = 'translateX(' + -slideSize * counter + 'px';
+      }
+    } else if (direction == 'left') {
+      counter--;
+      wrapper.style.transform = 'translateX(' + slideSize * counter + 'px';
 
-function slidingOn(direction) {
-  track.style.transition = "transform 0.4s ease-in-out";
-  if (direction == "right") {
-    counter++;
-    track.style.transform = "translateX(" + slideSize * counter + "px";
-    
-
-    if (counter === slidesCount) {
-      counter = slides.length - slidesCount;
-      track.style.transform = "translateX(" + -slideSize * counter + "px";
-    }
-  } else if (direction == "left") {
-    counter--;
-    track.style.transform = "translateX(" + slideSize * counter + "px";
-
-    if (counter === -1) {
-      counter = slides.length - 1;
-      track.style.transform = "translateX(" + slideSize * counter + "px";
+      if (counter === -1) {
+        counter = slides.length - 1;
+        wrapper.style.transform = 'translateX(' + slideSize * counter + 'px';
+      }
     }
   }
 }
+
+// creating a function that creates the arrow accepting an argument, based on which arrow we are setting
